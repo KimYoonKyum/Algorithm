45b03ae492741efbd46cb73886b21250818d1bf9
kimyoonkyeom <kimyoonkyeom@216.157.88.27>
2019-03-06 18:38:04 +0900

    챕터5

---
+++ algorithm/src/example/chapter5/Q2751.java
@@ -0,0 +1,27 @@
+package example.chapter5;
+
+import java.util.Scanner;
+
+public class Q2751 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int idx = 0;
+		for(int i=0; i<n;i++){
+			arr[idx] = sc.nextInt();
+		}
+	}
+	
+	//분할 하는 부분 
+	private static void mergeSort(int[] arr) {
+		
+	}
+	
+	//합병하는 부분 
+	private static void merge() {
+		
+	}
+
+}
